56631ed424f5825895552a4f4d9824a1551f12ca
Devansh Sharma <2022bit050@sggs.ac.in>
2024-03-23 14:59:24 +0530

    added practical 9 (complex)

---
+++ practical9complex.java
@@ -0,0 +1,59 @@
+class Complex {
+    double real, imaginary;
+
+    Complex() {
+    }
+
+    Complex(double tempReal, double tempImaginary) {
+        real = tempReal;
+        imaginary = tempImaginary;
+    }
+
+    Complex addComp(Complex C1, Complex C2) {
+        Complex temp = new Complex();
+        temp.real = C1.real + C2.real;
+        temp.imaginary = C1.imaginary + C2.imaginary;
+        return temp;
+    }
+
+    Complex subtractComp(Complex C1, Complex C2) {
+        Complex temp = new Complex();
+        temp.real = C1.real - C2.real;
+        temp.imaginary = C1.imaginary - C2.imaginary;
+        return temp;
+    }
+
+    Complex multiplyComp(Complex C1, Complex C2) {
+        Complex temp = new Complex();
+        temp.real = C1.real * C2.real ;
+        temp.imaginary = C1.imaginary * C2.imaginary ;
+        return temp;
+    }
+
+    void printComplexNumber() {
+        System.out.println("Complex number: " + real + " + " + imaginary + "i");
+    }
+}
+
+public class ComplexOperations {
+    public static void main(String[] args) {
+        Complex C1 = new Complex(1, 10);
+        C1.printComplexNumber();
+
+        Complex C2 = new Complex(10, 1);
+        C2.printComplexNumber();
+
+        Complex C3 = new Complex();
+        C3 = C3.addComp(C1, C2);
+        System.out.print("Sum of ");
+        C3.printComplexNumber();
+
+        C3 = C3.subtractComp(C1, C2);
+        System.out.print("Difference of ");
+        C3.printComplexNumber();
+
+        C3 = C3.multiplyComp(C1, C2);
+        System.out.print("Product of ");
+        C3.printComplexNumber();
+    }
+}
